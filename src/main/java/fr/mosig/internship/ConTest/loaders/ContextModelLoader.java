87f1ad3316115e4b8e1118ad35622971be198914
asadwan <asadwan@icloud.com>
2021-05-17 20:18:42 +0200

    added dev and context model loaders

--- src/main/java/fr/mosig/internship/ConTest/loaders/ContextModelLoader.java
+++ src/main/java/fr/mosig/internship/ConTest/loaders/ContextModelLoader.java
@@ -1,25 +1,27 @@
 package fr.mosig.internship.ConTest.loaders;
 
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.resource.XtextResourceSet;
-import fr.mosig.internship.chsm.*;
 
 import com.google.inject.Injector;
 
-public class DevModelLoader implements ModelLoader {
+import fr.mosig.internship.cdl.CdlStandaloneSetup;
+
+
+public class ContextModelLoader implements ModelLoader {
 
 	@Override
 	public EObject load(String path) {
 		new org.eclipse.emf.mwe.utils.StandaloneSetup().setPlatformUri("../");
-		Injector injector = new ChsmStandaloneSetup().createInjectorAndDoEMFRegistration();
+		Injector injector = new CdlStandaloneSetup().createInjectorAndDoEMFRegistration();
 		XtextResourceSet resourceSet = injector.getInstance(XtextResourceSet.class);
 		resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
 		Resource resource = resourceSet.getResource(
 		    URI.createURI(path), true);
-		return resource.getContents().get(0);		
+		return resource.getContents().get(0);	
 	}
 
 }
